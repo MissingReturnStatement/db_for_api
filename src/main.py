c640b17b2cfd46019f8b73c4eb2b5166d5fcb44e
Романов Денис <mr.deaddude@mail.ru>
2024-08-08 15:07:09 +0700

    Переделал создание таблиц

--- src/main.py
+++ src/main.py
@@ -2,12 +2,13 @@ import os
 import sys
 import asyncio
 sys.path.insert(1,os.path.join(sys.path[0],'..'))
-from queries.orm import async_insert_data,create_tables
+from queries.orm import SyncORM
+#from queries.orm import async_insert_data,#create_tables
 from database import sync_engine
 from models import metadata_object
 
 
-create_tables()
+SyncORM.create_tables()
 # metadata_object.reflect(bind=sync_engine)
 # tables=metadata_object.tables.keys()
 # for table in tables:
--- src/queries/orm.py
+++ src/queries/orm.py
@@ -1,26 +1,33 @@
-from sqlalchemy import text,insert
+from sqlalchemy import Integer, and_, cast, func, insert, inspect, or_, select, text
 from database import sync_engine, async_engine, session_factory, async_session_factory
 from models import metadata_object, EmbeddingsOrm, CompairedPairOrm, Base
-def create_tables():
-    #sync_engine.echo = False
-    Base.metadata.drop_all(sync_engine)
-    Base.metadata.create_all(sync_engine)
-    sync_engine.echo = True
 
-def select_all():
-    with session_factory() as session:
-        session.get()
-# def drop_all_tables(engine, metadata):
-#     with engine.connect() as conn:
-#         # Удаляем все зависимости через каскадное удаление
-#         conn.execute(text("DROP TABLE IF EXISTS model_results CASCADE"))
-#         conn.execute(text("DROP TABLE IF EXISTS embeddings CASCADE"))
+class SyncORM:
+    @staticmethod
+    def create_tables():
+        #sync_engine.echo = False
+        Base.metadata.drop_all(sync_engine)
+        Base.metadata.create_all(sync_engine)
+        sync_engine.echo = True
+    @staticmethod
+    def select_all_from_embeddings():
+        with session_factory() as session:
+            query = select(EmbeddingsOrm)
+            result = session.execute(query)
+            embeddings = result.scalars().all()
+            print(f"{embeddings}")
 
-def insert_data():
-    embed = EmbeddingsOrm(embedding = "some vector")
-    with session_factory() as session:
-        session.add(embed)
-        session.commit()
+    @staticmethod
+    def insert_test_data():
+        embed = EmbeddingsOrm(embedding = "some vector")
+        with session_factory() as session:
+            session.add(embed)
+            session.commit()
+
+    @staticmethod
+    def insert_embeddings():
+        with session_factory() as session:
+            pass
 
 async def async_insert_data():
     async with async_session_factory() as session:
