3b228cef81a865c2aec7e6bfe1f9222f17942cf6
Романов Денис <mr.deaddude@mail.ru>
2024-08-17 11:44:34 +0700

    Добавлен insert с проверкой хэша в таблицу embeddings,select по id и hash   из таблицы embeddings, delete по id и hash из таблицы embeddings

--- src/queries/orm.py
+++ src/queries/orm.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Integer, and_, cast, func, insert, inspect, or_, select, text
+from sqlalchemy import Integer, and_, cast, func, insert, inspect, or_, select, text,delete
 from database import sync_engine, async_engine, session_factory, async_session_factory
 from models import metadata_object, EmbeddingsOrm, CompairedPairOrm, Base
 import datetime
@@ -18,25 +18,71 @@ class SyncORM:
             for embedding in results:
                 print(f"ID: {embedding.id}, Embedding: {embedding.embedding}, Hash: {embedding.hash}, Repo: {embedding.repo}, Date: {embedding.date}")
 
+
     @staticmethod
-    def insert_test_data(embed_data):
+    def select_by_id_from_embeddings(id: int):
         with session_factory() as session:
+            statement = select(EmbeddingsOrm).where(EmbeddingsOrm.id == id)
+            result = session.execute(statement).scalar_one_or_none()
+            if result:
+                embedding_dict = {
+                    'id': result.id,
+                    'embedding': result.embedding,
+                    'hash': result.hash,
+                    'repo': result.repo,
+                    'date': result.date
+                }
+                return embedding_dict
+            else:
+                return None
+    @staticmethod
+    def select_by_hash_from_embeddings(hash: int):
+        with session_factory() as session:
+            statement = delete(EmbeddingsOrm).where(EmbeddingsOrm.hash == hash)
+            result = session.execute(statement).scalar_one_or_none()
+            if result:
+                embedding_dict = {
+                    'id': result.id,
+                    'embedding': result.embedding,
+                    'hash': result.hash,
+                    'repo': result.repo,
+                    'date': result.date
+                }
+                return embedding_dict
+            else:
+                return None
+
+    @staticmethod
+    def insert_data_to_embeddings(embed_data: dict):
+       if embed_data['hash'] != SyncORM.select_by_hash()['hash']:
+           with session_factory() as session:
             embed_obj = EmbeddingsOrm(
                 embedding=embed_data['embedding'],
                 hash=embed_data['hash'],
-                repo=embed_data.get('repo'),  # Используем get чтобы безопасно достать значение
-                date=embed_data.get('date', datetime.datetime.now())  # Дата по умолчанию текущее время
+                repo=embed_data.get('repo'),
+                date=embed_data.get('date', datetime.datetime.now())
             )
             session.add(embed_obj)
             session.commit()
-
+       else:
+           return SyncORM.select_by_hash()['hash']
     @staticmethod
-    def insert_embeddings():
+    def delete_by_id_from_embeddings(id: int):
         with session_factory() as session:
-            pass
+            embed = session.query(EmbeddingsOrm).filter(EmbeddingsOrm.id == id).first()
+            if embed:
+                session.delete(embed)
+                session.commit()
+            else:
+                pass
+                #можно какое-то логирование добавить
+    @staticmethod
+    def delete_by_hash_from_embeddings(hash: int):
+        with session_factory() as session:
+            embed = session.query(EmbeddingsOrm).filter(EmbeddingsOrm.hash == hash).first()
+            if embed:
+                session.delete(embed)
+                session.commit()
+            else:
+                pass
 
-async def async_insert_data():
-    async with async_session_factory() as session:
-        embed = EmbeddingsOrm(embedding="some vector")
-        session.add(embed)
-        await session.commit()
